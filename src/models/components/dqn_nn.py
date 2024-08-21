4580f981b2174d29d653d62aa1fbd4ee50352d83
snknitin <snk.nitin@gmail.com>
2024-08-21 14:45:36 +0530

    FC based change in sub action dimension

--- src/models/components/dqn_nn.py
+++ src/models/components/dqn_nn.py
@@ -115,21 +115,31 @@ class BranchDuelingDQN(nn.Module):
 
 
 class BranchDuelingDQNMulti(nn.Module):
-    def __init__(self, obs_size: int, n_actions: int, num_fcs: int, hidden_size: int = 64):
+    def __init__(self, obs_size: int, n_actions: int, num_fcs: int, hidden_size: int = 128,middle_size: int = 64,out_size: int = 32):
         super().__init__()
         self.num_fcs = num_fcs
+        self.n_actions = n_actions
+        self.obs_size = obs_size * num_fcs
         self.feature = nn.Sequential(
-            nn.Linear(obs_size, hidden_size),
+            nn.Linear(self.obs_size, hidden_size),
             nn.ReLU(),
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU()
+            # Dropout(p=0.2),
+            nn.Linear(hidden_size, middle_size),
+            nn.ReLU(),
+            # Dropout(p=0.2),
+            nn.Linear(middle_size, middle_size),
+            nn.ReLU(),
+            # Dropout(p=0.2),
+            nn.Linear(middle_size, out_size),
+            nn.ReLU(),
+            # Dropout(p=0.2)
         )
-        self.value = nn.Linear(hidden_size, num_fcs)
-        self.advantage = nn.Linear(hidden_size, num_fcs * n_actions)
+        self.value = nn.Linear(out_size, self.num_fcs)
+        self.advantage = nn.Linear(out_size, self.num_fcs * self.n_actions)
 
     def forward(self, x):
         x = x.float()
         feature = self.feature(x)
         value = self.value(feature).unsqueeze(-1)
-        advantage = self.advantage(feature).view(-1, self.num_fcs, 20)
+        advantage = self.advantage(feature).view(-1, self.num_fcs, self.n_actions)
         return value + advantage - advantage.mean(dim=-1, keepdim=True)

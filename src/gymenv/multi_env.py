ae1db465ad173421241a54c4a04a9c1575dd8fda
snknitin <snk.nitin@gmail.com>
2024-11-25 11:33:12 +0530

    Multi env changes

--- src/gymenv/multi_env.py
+++ src/gymenv/multi_env.py
@@ -1,5 +1,5 @@
 import pickle
-
+import cvxpy as cp
 import gym
 from gym import spaces
 import numpy as np
@@ -12,7 +12,6 @@ import ot
 
 ot.utils.check_random_state(seed=42)
 from sklearn.preprocessing import MinMaxScaler
-
 
 
 class MultiFCEnvironment(gym.Env):
@@ -74,6 +73,7 @@ class MultiFCEnvironment(gym.Env):
         self.Exp_demand = self.generate_demand_data()
         self.prod_sales = self.generate_act_sales_data()  # (266,59) wm_wk, 58 geos
         self.start_time_index = self.Exp_demand[self.Exp_demand['wm_yr_wk'] == self.starting_week].index[0]  # 119
+        self.test_time_index = self.prod_sales[self.prod_sales['wm_yr_wk'] == self.test_week].index[0]  # 203
 
         ## Sample Realised Demand from selected Regions (According to PR)
         curr_week_no = self.dem_ref.iloc[self.start_time_index, 0]  # 12351
@@ -104,11 +104,13 @@ class MultiFCEnvironment(gym.Env):
         # self.num_regions = len(self.geo_ids_list)
         self.forecast_horizon = env_cfg.get('forecast_horizon', 100)
         self.starting_week = env_cfg.get('starting_week', 12351)
+        self.test_week = env_cfg.get('test_week', 12411)
+
         self.sel_fcs = env_cfg.get('sel_FCs', [4270, 6284, 6753])
         sel_fcs = [int(fc_id) for fc_id in self.sel_fcs]
         self.sel_fcs = sel_fcs
         self.min_max_scaler = MinMaxScaler()
-
+        self.compute_sla = False
 
     def setup_state_space(self):
         lt_ecdfs = [self.fc_leadtimes.get(fc_id) for fc_id in self.sel_fcs]
@@ -122,8 +124,8 @@ class MultiFCEnvironment(gym.Env):
     def setup_action_space(self):
         # self.action_space = spaces.Box(low=0, high=20, shape=(self.num_fc, self.num_sku), dtype=np.float32)
         # self.action_space = spaces.MultiDiscrete([20] * self.num_fcs)
-        self.action_space = spaces.Box(low=0, high=3, shape=(self.num_fcs, self.num_sku),
-                                       dtype=np.float32)  # Define action space based on the problem
+        self.action_space = spaces.Box(low=0, high=5, shape=(self.num_fcs, self.num_sku),
+                                       dtype=np.float32)  # Define action space based on the problem - high is getting set manually
 
     def reset(self, seed: Optional[int] = None, options: Optional[dict] = None):
         super().reset(seed=seed)
@@ -136,6 +138,7 @@ class MultiFCEnvironment(gym.Env):
 
         new_seed = self.base_seed + self.reset_count
         torch.manual_seed(new_seed)
+        # np.random.seed(new_seed)
 
         # Someway to get timestamp in state to go back to zero
         # Update the seed and reinitialize the existing state space
@@ -145,21 +148,42 @@ class MultiFCEnvironment(gym.Env):
 
         # Check if 'test' is in options and if it's True
         if options.get('actual_test', False):  # Default to False if 'test' is not present
-            self.realized_demand_all = self.prod_sales[self.sel_regions]
+            self.compute_sla = True
+            self.realized_demand_all = self.prod_sales[self.sel_regions][self.test_time_index:]
+            self.realized_demand_sel = self.realized_demand_all[self.sel_regions]  # (147,14)
+
+            mapped_forecast = np.array(self.exp_dem_sel.iloc[:self.forecast_horizon]) @ self.curr_pl_ratio
+            mapped_forecast = np.vstack([mapped_forecast.values, np.tile(mapped_forecast.values[-1, np.newaxis],
+                                                                         (self.forecast_horizon - mapped_forecast.shape[
+                                                                             0], 1))]) if mapped_forecast.shape[
+                                                                                              0] < self.forecast_horizon else mapped_forecast.values
+            self.inventory_state.set_forecast(torch.from_numpy(mapped_forecast.T))
+
         elif options.get('simulated_test', False):
+            self.compute_sla = True
             # Add random noise
             mean_noise, std_dev_noise = options['simulated_test']
             self.exp_demand = (
-                        self.exp_demand + np.random.normal(mean_noise, std_dev_noise, self.exp_demand.shape)).clip(
+                    self.exp_demand + np.random.normal(mean_noise, std_dev_noise, self.exp_demand.shape)).clip(
                 lower=0)
             self.realized_demand_all = np.ceil(self.gen_realised_dem(self.exp_demand))
+            self.realized_demand_sel = self.realized_demand_all[self.sel_regions]  # (147,14)
+
+            mapped_forecast = np.array(self.exp_dem_sel.iloc[:self.forecast_horizon]) @ self.curr_pl_ratio
+            mapped_forecast = np.vstack([mapped_forecast.values, np.tile(mapped_forecast.values[-1, np.newaxis],
+                                                                         (self.forecast_horizon - mapped_forecast.shape[
+                                                                             0], 1))]) if mapped_forecast.shape[
+                                                                                              0] < self.forecast_horizon else mapped_forecast.values
+            self.inventory_state.set_forecast(torch.from_numpy(mapped_forecast.T))
         else:
             ## For selecting regions mapped to selected FCs==============================================================
             self.realized_demand_all = np.ceil(self.gen_realised_dem(self.exp_demand))
+            self.realized_demand_sel = self.realized_demand_all[self.sel_regions]  # (147,14)
+
+            mapped_forecast = np.array(self.exp_dem_sel.iloc[:self.forecast_horizon]) @ self.curr_pl_ratio
+            self.inventory_state.set_forecast(torch.from_numpy(mapped_forecast.values.T))
             ##  =========================================================================================================
 
-        mapped_forecast = np.array(self.exp_dem_sel.iloc[:self.forecast_horizon]) @ self.curr_pl_ratio
-        self.inventory_state.set_forecast(torch.from_numpy(mapped_forecast.values.T))
         self.state, self.multiplier = self.inventory_state.get_state()
         self.reset_count += 1
         return self.state
@@ -179,19 +203,20 @@ class MultiFCEnvironment(gym.Env):
         inventory_at_beginning_of_day = self.state[0::fc_st_dim].reshape(-1, self.num_fcs)
         inventory_after_replenishment = self.state[3::fc_st_dim].reshape(-1, self.num_fcs)
 
-        # reward, sales_at_FC, holding_cost, shortage_cost, mapped_demand = self.calculate_reward(
-        #     inventory_after_replenishment, realized_demand, curr_pl_ratio)
-
         mapped_demand_pr = np.array(realized_demand.reshape(1, -1) @ self.curr_pl_ratio).flatten()
         # mapped_demand_pr = np.ceil(mapped_demand_pr)
 
         # multiplier = self.multiplier.copy().reshape(self.num_fcs,-1)
         multiplier = self.multiplier.clone().reshape(self.num_fcs, -1)
 
-        reward, sales_at_FC, holding_cost_pr, shortage_cost_pr, mapped_dem_ot, holding_qty_pr, shortage_qty_pr, holding_qty_ot, shortage_qty_ot = self.calculate_reward_OT(
-            inventory_after_replenishment, realized_demand, mapped_demand_pr, self.benefits_data_table)
+        reward, sales_at_FC, holding_cost_pr, shortage_cost_pr, mapped_dem_ot, holding_qty_pr, shortage_qty_pr, holding_qty_ot, shortage_qty_ot, uot_plan, x = self.calculate_reward_OT(
+            inventory_after_replenishment, realized_demand, mapped_demand_pr, self.benefits_data_table,
+            self.compute_sla)
 
-        assert mapped_demand_pr.sum() >= mapped_dem_ot.sum(), "f{t}, PR Not Equal to OT"
+        # reward, sales_at_FC, holding_cost_pr, shortage_cost_pr, mapped_demand_pr = self.calculate_reward(
+        #     inventory_after_replenishment, realized_demand, mapped_demand_pr)
+
+        # assert mapped_demand_pr.sum() >= mapped_dem_ot.sum(), "f{t}, PR Not Equal to OT"
 
         repl_received = inventory_after_replenishment - inventory_at_beginning_of_day
         inventory_at_end_of_day = inventory_after_replenishment - sales_at_FC
@@ -199,7 +224,7 @@ class MultiFCEnvironment(gym.Env):
         self.inventory_state.update(sales=sales_at_FC.flatten(),
                                     actions=action.flatten(),
                                     multiplier=multiplier.flatten(),
-                                    repl_received= repl_received.flatten())
+                                    repl_received=repl_received.flatten())
 
         self.state, self.multiplier = self.inventory_state.get_state()
 
@@ -212,25 +237,29 @@ class MultiFCEnvironment(gym.Env):
             'realised_dem': realized_demand.flatten(),
             'inv_at_end_of_day': inventory_at_end_of_day.flatten(),
             'sales_at_FC': sales_at_FC.flatten(),
-            # 'holding_cost': holding_cost.flatten(),
-            # 'shortage_cost': shortage_cost.flatten(),
             'holding_cost': holding_cost_pr.flatten(),
             'shortage_cost': shortage_cost_pr.flatten(),
-            'repl_ord': (action * multiplier).flatten(),
+            'holding_qty_pr': holding_qty_pr.flatten(),
+            'shortage_qty_pr': shortage_qty_pr.flatten(),
+            'holding_qty_ot': holding_qty_ot.flatten(),
+            'shortage_qty_ot': shortage_qty_ot.flatten(),
+            'repl_ord': action.flatten() * multiplier.flatten(),
             'action': action.flatten(),
             'multiplier': multiplier.flatten(),
             'mapped_dem': mapped_demand_pr.flatten(),
             'repl_rec': repl_received.flatten(),
-            'mapped_forecast': self.mapped_forecast_plotting
+            'mapped_forecast': self.mapped_forecast_plotting,
+            'uot_plan': uot_plan,
+            'adjusted_inventory_sla': x
         }
 
         return self.state, reward, done, info
 
-
-    def calculate_reward(self, inventory, demand, pl_ratio):
+    def calculate_reward(self, inventory, demand, mapped_demand):
         # Ensure all inputs are tensors on the correct device
-        mapped_demand = torch.mm(demand.T, pl_ratio)[:, :self.num_fcs]
-
+        # mapped_demand = torch.mm(demand.T, pl_ratio)[:, :self.num_fcs]
+        demand = torch.from_numpy(demand)
+        mapped_demand = torch.from_numpy(mapped_demand)
         sales = torch.min(inventory, mapped_demand)
         holding_cost = self.hc * torch.clamp(inventory - mapped_demand, min=0)
         shortage_cost = self.sc * torch.clamp(mapped_demand - inventory, min=0)
@@ -242,19 +271,18 @@ class MultiFCEnvironment(gym.Env):
         reward = torch.sum(reward, dim=0)  # Shape: (num_fcs,)
 
         # For Scalar Reward, uncomment the following line:
-        # reward = torch.sum(reward)
+        reward = torch.sum(reward)
 
         return reward, sales, holding_cost, shortage_cost, mapped_demand
 
-    def calculate_reward_OT(self, inventory, realized_demand, mapped_dem_pr, benefits_data_table):
+    def calculate_reward_OT(self, inventory, realized_demand, mapped_dem_pr, benefits_data_table, compute_sla):
         # Move all inputs to the correct device
         inventory = inventory.reshape(-1, 1)
         realized_demand = torch.from_numpy(realized_demand)
         mapped_dem_pr = torch.from_numpy(mapped_dem_pr)
         # benefits_data_table = torch.tensor(benefits_data_table)
 
-
-        temp_realized_demand = torch.cat([realized_demand, torch.zeros((1, self.num_sku))],dim=0).int()
+        temp_realized_demand = torch.cat([realized_demand, torch.zeros((1, self.num_sku))], dim=0).int()
         temp_inventory = torch.cat([inventory, torch.zeros((1, self.num_sku))], dim=0).int()
 
         demand_sums = torch.sum(temp_realized_demand[:-1, :], dim=0)
@@ -271,42 +299,75 @@ class MultiFCEnvironment(gym.Env):
         holding_cost_pr, shortage_cost_pr = 0, 0
 
         for i in range(temp_inventory.shape[1]):
-            M = np.array(benefits_data_table)
-            M = 1 - self.min_max_scaler.fit_transform(M.T).T
-            M = torch.tensor(M)
+            ### M will be used for cost matrix in OT as well as for optimisation function for calculating SLA
+            ### SLA calculations not yet committed
+            benefits_data_table_array = np.array(benefits_data_table)
+            M = 1 - self.min_max_scaler.fit_transform(benefits_data_table_array)
+
+            #### For SLA Computation
+            ### Use this only during testing:
+            x = np.zeros((self.num_fcs))
+            if compute_sla:
+                min_val, max_val = np.min(benefits_data_table_array), np.max(benefits_data_table_array)
+                M1 = 1.0 - ((benefits_data_table_array - min_val) / (max_val - min_val))
+                M = 1.0 - ((benefits_data_table_array - min_val) / (max_val - min_val))
+                alpha, beta = 0.1, 1000
+                num_regions = M1.shape[1]
+                tp = cp.Variable((self.num_fcs, num_regions), nonneg=True)  # Transport plan
+                x = cp.Variable(self.num_fcs)  # Inventory adjustment variable
+                # Objective
+                objective = cp.Minimize(alpha * cp.sum(cp.maximum(0, x)) + beta * cp.sum(cp.multiply(tp, M1)))
+                # Constraints
+                t1 = realized_demand.to('cpu').numpy().reshape(1, -1)
+                t2 = inventory.to('cpu').numpy().reshape(1, -1)
+                constraints = [cp.sum(tp, axis=0) == t1, cp.sum(tp, axis=1) == t2 + x]
+                # problem
+                prob = cp.Problem(objective, constraints)
+                prob.solve()
+                optimization_fulfilment_cost = np.sum(np.multiply(tp.value, M1))
+                x = x.value
 
             benefits = 1 - M
-
-            M = torch.cat([M, torch.ones((1, M.shape[1])) * self.sc * 5], dim=0)
-            M = torch.cat([M, torch.ones((M.shape[0], 1)) * self.hc * 5], dim=1)
+            M = torch.tensor(M)
+            M = torch.cat([M, torch.ones((1, M.shape[1])) * self.sc], dim=0)
+            M = torch.cat([M, torch.ones((M.shape[0], 1)) * self.hc], dim=1)
 
             supply = temp_inventory[:, i].cpu().numpy()
             demand = temp_realized_demand[:, i].cpu().numpy()
 
-            uot = ot.solve(M.cpu().numpy() / M.max().item(), supply.astype('float'), demand.astype('float'),
+            # scaling costs for uot
+            uot_scale_factor = M.max()
+            uot = ot.solve(M.cpu().numpy() / uot_scale_factor.item(), supply.astype('float'), demand.astype('float'),
                            method='emd')
 
             sales_at_FC = torch.tensor(uot.plan[:-1, :-1].sum(axis=1))
             mapped_dem = sales_at_FC.int()
 
             # (OT) Calculate reward
-            holding_cost_ot += torch.clamp(temp_inventory[:-1, i].sum(), min=0) * self.hc
+            holding_cost_ot += torch.clamp(temp_inventory[:-1, i].sum(), min=0) * self.hc * uot_scale_factor
             holding_qty_ot = torch.clamp(-differences[i], min=0)
-            shortage_cost_ot += torch.clamp(differences[i], min=0) * self.sc
+            shortage_cost_ot += torch.clamp(differences[i], min=0) * self.sc * uot_scale_factor
             shortage_qty_ot = torch.clamp(differences[i], min=0)
 
             # (PR) Calculate reward
             diff = mapped_dem_pr.flatten() - inventory.flatten()
-            holding_cost_pr = torch.where(diff > 0, torch.tensor(0.), diff * -self.hc * 5) / M.max()
-            holding_qty_pr = torch.sum(torch.where(diff > 0, torch.tensor(0.), -diff))
-            shortage_cost_pr = torch.where(diff > 0, diff * self.sc * 5, torch.tensor(0.)) / M.max()
-            shortage_qty_pr = torch.sum(torch.where(diff > 0, diff, torch.tensor(0.)))
+            holding_cost_pr = torch.where(diff > 0, torch.tensor(0.), diff * -self.hc)
+            holding_qty_pr = torch.where(diff > 0, torch.tensor(0.), -diff)
+            # holding_qty_pr = torch.sum(torch.where(diff > 0, torch.tensor(0.), -diff))
+
+            shortage_cost_pr = torch.where(diff > 0, diff * self.sc, torch.tensor(0.))
+            shortage_qty_pr = torch.where(diff > 0, diff, torch.tensor(0.))
+            # shortage_qty_pr = torch.sum(torch.where(diff > 0, diff, torch.tensor(0.)))
+
+            price = self.sc * 5
+            sales_revenue = price * sales_at_FC
 
             local_cost = torch.sum(shortage_cost_pr + holding_cost_pr)
-            global_cost = uot.value
+            global_cost = uot.value * uot_scale_factor.item()
 
             reward = -1 * (0 * global_cost + 1 * local_cost)
 
-        return reward, sales_at_FC, holding_cost_pr, shortage_cost_pr, mapped_dem, holding_qty_pr, shortage_qty_pr, holding_qty_ot, shortage_qty_ot
+            # All scaling for holding & shoratge is removed
+            reward = reward / uot_scale_factor.item()  # --> Rescaled to be in sync with older reward - this is temporary & needs to be removed
 
-
+        return reward, sales_at_FC, holding_cost_pr, shortage_cost_pr, mapped_dem, holding_qty_pr, shortage_qty_pr, holding_qty_ot, shortage_qty_ot, uot.plan, x
